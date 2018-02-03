7bd9541c50c0062327ba392b0ca1411463461a64
njang <neo.w.jang@gmail.com>
2018-02-03 01:11:43 -0600

    Disable registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+// import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+	<BrowserRouter>
+	  <App />
+	</BrowserRouter>
+), document.getElementById('root'));
+// registerServiceWorker();
