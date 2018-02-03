185ed300abedae34cd22929a647c74485f123a0e
njang <neo.w.jang@gmail.com>
2018-02-02 23:33:07 -0600

    Remove font-awesome package

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './Header.css';
 import logo from '../TAM-Logo-white.svg';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/fontawesome-free-brands'
 
 class Header extends Component {
   render() {
     return (
   		<header className="App-header row">
         <h1 className="App-title col col-8 text-uppercase font-weight-bold align-middle">
-        	<FontAwesomeIcon icon="faCoffee" />
         	BGCC
         </h1>
         <img src={logo} className="App-logo col col-4" alt="logo" />
