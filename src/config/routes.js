42dab0e3411d0d2642cb554fe47e982339f30164
njang <neo.w.jang@gmail.com>
2018-02-03 01:23:55 -0600

    Enable HashRouter in routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Eat from '../components/Eat';
 import Rest from '../components/Rest';
 import Play from '../components/Play';
@@ -7,13 +7,15 @@ import Potty from '../components/Potty';
 import Incident from '../components/Incident';
 
 export default (
-  <Switch>
-    {/*<Route exact path='/' component={ Home } />*/}
-    <Route path='/eat' component={ Eat } />
-    <Route path='/rest' component={ Rest } />
-    <Route path='/play' component={ Play } />
-    <Route path='/potty' component={ Potty } />
-    <Route path='/incidents' component={ Incident } />
-  </Switch>
+	<HashRouter>
+  	<Switch>
+	    {/*<Route exact path='/' component={ Home } />*/}
+	    <Route path='/eat' component={ Eat } />
+	    <Route path='/rest' component={ Rest } />
+	    <Route path='/play' component={ Play } />
+	    <Route path='/potty' component={ Potty } />
+	    <Route path='/incident' component={ Incident } />
+	  </Switch>
+	</HashRouter>
 )
 
