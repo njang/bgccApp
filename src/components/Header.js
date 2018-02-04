51d2de181087f2462a7e2c72d41d4e3772611028
njang <neo.w.jang@gmail.com>
2018-02-03 23:43:39 -0600

    Enable font awesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './Header.css';
 import logo from '../TAM-Logo-white.svg';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/fontawesome-free-solid'
 
 class Header extends Component {
   render() {
     return (
   		<header className="App-header row">
         <h1 className="App-title col col-8 text-uppercase font-weight-bold align-middle">
-        	BGCC
+          <FontAwesomeIcon icon={ faBars } />
+          BGCC
         </h1>
         <img src={logo} className="App-logo col col-4" alt="logo" />
 
