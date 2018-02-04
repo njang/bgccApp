ab535e55ba7bf975a5c23641fc69e7ddf588f75c
njang <neo.w.jang@gmail.com>
2018-02-03 23:29:03 -0600

    Remove npm fortawesome

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render((
-	<HashRouter>
+	<BrowserRouter>
 	  <App />
-	</HashRouter>
+	</BrowserRouter>
 ), document.getElementById('root'));
 // registerServiceWorker();
