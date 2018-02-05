5cb201edd5c3b89e0fcaca39c30125dd47018222
njang <neo.w.jang@gmail.com>
2018-02-05 12:15:40 -0600

    Add student model

---
+++ src/components/Student.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Student extends Component {
+  render() {
+    return (
+    	<div className="studentCard">
+    		Photo
+    		Name
+    		DOB
+    	</div>
+  	);
+  }
+}
+
+export default Student;
--- src/containers/Students.js
+++ src/containers/Students.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Student from '../components/Student'
 
 class Students extends Component {
   render() {
     return (
       <div className="studentsList">
 				Student list goes here
+				<Student />
       </div>
     )
   }
