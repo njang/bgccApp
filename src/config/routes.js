359b4327ac187d0d6697bbdf2dcbfe6ffd67e853
njang <neo.w.jang@gmail.com>
2018-02-08 21:41:45 -0600

    Separate out add child form

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ChildrenContainer from '../containers/ChildrenContainer'
 import ChildContainer from '../containers/ChildContainer'
+import AddChildForm from '../components/AddChild'
 import Eat from '../components/Eat';
 import Rest from '../components/Rest';
 import Play from '../components/Play';
@@ -12,9 +13,8 @@ export default (
 	<Switch>
     <Route exact path='/' component={ ChildrenContainer } />
   	<Route exact path='/children' component={ ChildrenContainer } />
+  	<Route exact path='/children/add' component={ AddChildForm } />
   	<Route path='/children/:id' component={ ChildContainer } />
-
-
 
     <Route path='/eat' component={ Eat } />
     <Route path='/rest' component={ Rest } />
