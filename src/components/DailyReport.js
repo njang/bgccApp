e95cc537a2875a8c58d55b4cfeeb3832b0636d19
njang <neo.w.jang@gmail.com>
2018-02-13 09:18:07 -0600

    Restructure eat/rest documents

--- src/components/DailyReport.js
+++ src/components/DailyReport.js
@@ -1,43 +1,21 @@
 import React, {Component} from 'react'
 // import {Link} from 'react-router-dom';
 import './DailyReport.css';
+import Eat from './Eat'
+import Rest from './Rest'
 
 class DailyReport extends Component {
   render(){
     return(
-      <div className='row'>
-        <form className='col-md-4 col-sm-12 form-group'>
-          <label>AM Snacks</label>
-          <select class='form-control' id='snacksAM' aria-describedby='snacksAM' placeholder='Enter email'>
-            <option selected disabled hidden>--</option>
-            <option value='All'>Ate all</option>
-            <option value='Lot'>Ate lots</option>
-            <option value='Some'>Ate some</option>
-            <option value='None'>Ate none</option>
-          </select>
-        </form>
-        <form className='col-md-4 col-sm-12 form-group'>
-          <label>Lunch</label>
-          <select class='form-control' id='lunch' aria-describedby='lunch' placeholder='Enter email'>
-            <option selected disabled hidden>--</option>
-            <option value='All'>Ate all</option>
-            <option value='Lot'>Ate lots</option>
-            <option value='Some'>Ate some</option>
-            <option value='None'>Ate none</option>
-          </select>
-        </form>
-        <form className='col-md-4 col-sm-12 form-group'>
-          <label>PM Snacks</label>
-          <select class='form-control' id='snacksPM' aria-describedby='snacksPM' placeholder='Enter email'>
-            <option selected disabled hidden>--</option>
-            <option value='All'>Ate all</option>
-            <option value='Lot'>Ate lots</option>
-            <option value='Some'>Ate some</option>
-            <option value='None'>Ate none</option>
-          </select>
-        </form>
-        
-      </div>
+    	<div>
+    		<h2>Ate</h2>
+	      <div className='row'>
+	        <Eat meal='AM snack' id='snackAM' />
+	        <Eat meal='Lunch' id='lunch' />
+	        <Eat meal='PM snack' id='snackPM'/>
+	      </div>
+	      <Rest />
+	    </div>
     )
   }
 }
--- src/components/Eat.js
+++ src/components/Eat.js
@@ -3,40 +3,16 @@ import React, { Component } from 'react';
 class Eat extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <label>AM Snacks</label>
-          <select class="form-control" id="snacksAM" aria-describedby="emailHelp" placeholder="Enter email">
-            <option value="volvo">--</option>
-            <option value="volvo">Ate all</option>
-            <option value="saab">Ate a lot</option>
-            <option value="mercedes">Ate some</option>
-            <option value="audi">Ate none</option>
-          </select>
-        </form>
-
-        <form>
-          <label>Lunch</label>
-          <select class="form-control" id="snacksAM" aria-describedby="emailHelp" placeholder="Enter email">
-            <option value="volvo">--</option>
-            <option value="volvo">Ate all</option>
-            <option value="saab">Ate a lot</option>
-            <option value="mercedes">Ate some</option>
-            <option value="audi">Ate none</option>
-          </select>
-        </form>
-
-        <form>
-          <label>PM Snacks</label>
-          <select class="form-control" id="snacksAM" aria-describedby="emailHelp" placeholder="Enter email">
-            <option value="volvo">--</option>
-            <option value="volvo">Ate all</option>
-            <option value="saab">Ate a lot</option>
-            <option value="mercedes">Ate some</option>
-            <option value="audi">Ate none</option>
-          </select>
-        </form>
-      </div>
+      <form className='col-md-4 col-sm-10 offset-md-0 offset-sm-1 form-group'>
+        <label>{ this.props.meal }</label>
+        <select class='form-control' id={ this.props.id } aria-describedby={ this.props.id }>
+          <option selected disabled hidden>--</option>
+          <option value='All'>Ate all</option>
+          <option value='Lot'>Ate lots</option>
+          <option value='Some'>Ate some</option>
+          <option value='None'>Ate none</option>
+        </select>
+      </form>
     );
   }
 }
