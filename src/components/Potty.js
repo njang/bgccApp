6a84c9cc0fdcc31e707ad44ea4d2ffea0aa67d4d
njang <neo.w.jang@gmail.com>
2018-02-14 22:58:51 -0600

    Revise potty form

--- src/components/Potty.js
+++ src/components/Potty.js
@@ -5,19 +5,21 @@ class Potty extends Component {
     return (
       <div className='col-6'>
         <h2>Potty</h2>
-        <form className='row'>
-          <input type='time' className='form-control col-4' />
-          <select className='form-control col-3 offset-1' id='type' aria-describedby='type'>
-            <option selected disabled hidden>--</option>
-            <option value='dry'>Dry</option>
-            <option value='wet'>Wet</option>
-            <option value='BM'>BM</option>
-          </select>
-          <select className='form-control col-3' id='type' aria-describedby='type'>
-            <option selected disabled hidden>--</option>
-            <option value='tried'>Tried</option>
-            <option value='pottied'>Pottied</option>
-          </select>
+        <form className='form-group'>
+          <div clasName='row'>
+            <input className='form-control col-3' type='time' />
+            <select className='form-control col-3' id='type' aria-describedby='type'>
+              <option selected disabled hidden>--</option>
+              <option value='dry'>Dry</option>
+              <option value='wet'>Wet</option>
+              <option value='BM'>BM</option>
+            </select>
+            <select className='form-control col-3' id='type' aria-describedby='type'>
+              <option selected disabled hidden>--</option>
+              <option value='tried'>Tried</option>
+              <option value='pottied'>Pottied</option>
+            </select>
+          </div>
         </form>
 
         <label>Comments</label>
